d2725e2dc07752685c5ddb7b8240453d5b7b3250
Rabiul-Sujon <alamrabiul889@gmail.com>
2026-06-12 23:49:17 +0600

    feat: add Pricing page route

---
+++ src/app/pricing/page.jsx
@@ -0,0 +1,9 @@
+import Pricing from "@/components/Pricing";
+
+export default function PricingPage() {
+  return (
+    <div className="bg-black min-h-screen pt-20">
+      <Pricing />
+    </div>
+  );
+}
